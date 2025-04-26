74cf0a2e45d82b2b355e3b74449ddaefdf5d0a4e
M1cK3yM <michaelyirdaw652@gmail.com>
2025-04-26 19:13:21 +0300

    feat: drag and drop

--- internal/audio/audio.go
+++ internal/audio/audio.go
@@ -2,19 +2,40 @@ package audio
 
 import rl "github.com/gen2brain/raylib-go/raylib"
 
-var music rl.Music
+type Music struct {
+	music      rl.Music
+	play       bool
+	timePlayed float32
+}
+
+var m Music
+
+func GetTimePlayed() float32 {
+	return m.timePlayed
+}
 
 func InitMusic(path string) {
-	music = rl.LoadMusicStream(path)
-	rl.PlayMusicStream(music)
+	m.music = rl.LoadMusicStream(path)
+	rl.PlayMusicStream(m.music)
+	m.play = true
 }
 
 func UpdateMusicStream() {
-	rl.UpdateMusicStream(music)
+	rl.UpdateMusicStream(m.music)
+	m.timePlayed = rl.GetMusicTimePlayed(m.music) / rl.GetMusicTimeLength(m.music)
+}
+
+func TogglePlay() {
+	m.play = !m.play
+	if m.play {
+		rl.PlayMusicStream(m.music)
+	} else {
+		rl.PauseMusicStream(m.music)
+	}
 }
 
 func GetWaveData() []float32 {
-	samples := rl.GetMusicTimePlayed(music) // You'd expand to real wave sampling
+	samples := rl.GetMusicTimePlayed(m.music) // You'd expand to real wave sampling
 	return []float32{float32(samples)}
 }
 
--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"path/filepath"
+	"strings"
+
 	"github.com/M1cK3yM/musializer-go/internal/audio"
 	"github.com/M1cK3yM/musializer-go/internal/config"
 
@@ -13,14 +16,41 @@ func main() {
 	rl.SetTargetFPS(config.FPS)
 	defer rl.CloseWindow()
 
-	audio.InitMusic("/path/to/music")
-
 	for !rl.WindowShouldClose() {
+		if rl.IsKeyPressed(rl.KeySpace) {
+			audio.TogglePlay()
+		}
+
 		rl.BeginDrawing()
 		rl.ClearBackground(config.BackgroundColor)
 
-		audio.UpdateMusicStream()
+		rl.DrawRectangle(
+			(int32)(rl.GetScreenWidth()-(int)(float64(rl.GetScreenWidth())*0.8)),
+			int32(rl.GetScreenHeight())-50,
+			400, 12, rl.LightGray)
 
+		rl.DrawRectangle(
+			(int32)(rl.GetScreenWidth()-(int)(float64(rl.GetScreenWidth())*0.8)),
+			int32(rl.GetScreenHeight())-50,
+			(int32)(audio.GetTimePlayed()*400.0), 12, rl.Maroon)
+
+		rl.DrawRectangleLines(
+			(int32)(rl.GetScreenWidth()-(int)(float64(rl.GetScreenWidth())*0.8)),
+			int32(rl.GetScreenHeight())-50,
+			400, 12, rl.Gray)
+
+		if rl.IsFileDropped() {
+			paths := rl.LoadDroppedFiles()
+			for _, path := range paths {
+				if strings.ToLower(filepath.Ext(path)) == ".mp3" ||
+					strings.ToLower(filepath.Ext(path)) == ".wav" ||
+					strings.ToLower(filepath.Ext(path)) == ".flac" {
+					audio.InitMusic(path)
+				}
+			}
+		}
+
+		audio.UpdateMusicStream()
 		rl.EndDrawing()
 	}
 }
